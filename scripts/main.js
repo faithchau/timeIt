5275dfc37dd7e35c881f461ec979de8af8e237fb
Faith Chau <faith_chau@Faiths-MacBook-Pro.local>
2017-08-13 16:55:47 -0700

    more UI enhancement and small bug fixes

--- scripts/main.js
+++ scripts/main.js
@@ -17,26 +17,32 @@ function startIt(){
 	var min = document.getElementById("min").value;
 	var sec = document.getElementById("sec").value; 
 
-	if (min<0 || sec<0){
+	//console.log("min "+ min);
+	//console.log("sec "+sec);
+
+	if (min<0 || sec<0 || min == "" || sec == ""){
 		alert("Please enter valid time");
 	}else{
 
 	//convert to ms
-	var time = (min * 60000) + (sec * 1000); 
+		var time = (min * 60000) + (sec * 1000); 
 
 
-	var userName = firebase.auth().currentUser.displayName; 
+		var userName = firebase.auth().currentUser.displayName; 
 
-	//push to firebase database
-	firebase.database().ref('times').push({
-		time: time, 
-		fcmtoken: deviceToken,
-		user: userName || 'Hey there!'
-	}).catch(function(error){
-		console.error("Error writing to FB", error);
-	});
+		//push to firebase database
+		firebase.database().ref('times').push({
+			time: time, 
+			fcmtoken: deviceToken,
+			user: userName || 'Hey there!'
+		}).catch(function(error){
+			console.error("Error writing to FB", error);
+			alert("Sorry! something went wrong !");
+		});
 
 	}
+
+	document.getElementById("inputForm").reset();
 
 };
 
@@ -52,6 +58,7 @@ function saveMessagingDeviceToken (){
 
 		}.bind(this)).catch(function(error){
 			console.error("unable to get messaging token", error);
+			alert("Sorry! Something went wrong!");
 		});
 	};
 
@@ -72,16 +79,17 @@ function signIn(){
 
 	var provider = new firebase.auth.GoogleAuthProvider();
 	firebase.auth().signInWithPopup(provider).then(function(result){
-		document.getElementById("username").innerHTML = result.user.displayName; 
-		document.getElementById("username").removeAttribute('hidden');
-		document.getElementById("signOut").removeAttribute('hidden');
-		document.getElementById("signIn").setAttribute('hidden', true);
+		document.getElementById("username").innerHTML = 'Welcome back! '+result.user.displayName; 
+		//document.getElementById("username").removeAttribute('hidden');
+		document.getElementById("signedIn").removeAttribute('hidden');
+		document.getElementById("signedOut").setAttribute('hidden', true);
 		document.getElementById("startIt").removeAttribute('disabled');
 
 		saveMessagingDeviceToken();
 		
 	}).catch (function(error){
 		console.error("user sign in unsuccessful", error);
+		alert("Sorry! Something went wrong!");
 	}); 
 };
 
@@ -90,12 +98,13 @@ function signOut (){
 
 	firebase.auth().signOut().then(function(){
 		console.log('user signed out successful');
-		document.getElementById("username").setAttribute('hidden', true);
-		document.getElementById("signOut").setAttribute('hidden', true);
-		document.getElementById("signIn").removeAttribute('hidden');
-		document.getElementByid("startIt").setAttribute('disabled', true);
+		//document.getElementById("username").setAttribute('hidden', true);
+		document.getElementById("signedIn").setAttribute('hidden', true);
+		document.getElementById("signedOut").removeAttribute('hidden');
+		document.getElementById("startIt").setAttribute('disabled', true);
 	}).catch(function(error){
 		console.error ("user sign out unsuccessful", error);
+		alert("Sorry! Something went wrong!");
 	});
 };
 
